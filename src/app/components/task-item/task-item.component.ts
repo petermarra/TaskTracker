fc785e2daa602ba21f05d2c9616bc264fd494ead
Peter Marra <neverquit@roadrunner.com>
2021-08-15 17:03:37 -0400

    create delete event.

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -8,12 +8,17 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent implements OnInit {
-  @Input() task!: Task
+  @Input()
+  task!: Task;
   faTimes = faTimes;
   
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(task:any) {
+    console.log(task);
   }
 
 }
