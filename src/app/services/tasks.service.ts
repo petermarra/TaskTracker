7cbbdcda794bb13c5871dd4a33845a7fc45109c9
Peter Marra <neverquit@roadrunner.com>
2021-08-15 16:42:01 -0400

    add functionality to connect to mock server through angular httpclient. create get functionality.

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
-import {Observable, observable, of} from "rxjs";
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import {Observable, observable} from "rxjs";
 import { Task } from '../Task';
-import{TASKS} from '../mock-tasks';
+
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks'
+  constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
